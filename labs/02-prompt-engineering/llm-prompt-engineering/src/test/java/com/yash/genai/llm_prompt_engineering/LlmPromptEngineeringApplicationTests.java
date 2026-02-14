966cdc4dd193b322553b0918a7f58139865ecc1e
98001yash <yashchauhan.gaya@gmail.com>
2026-02-14 13:19:05 +0530

    added the prompt-engineering labs

---
+++ labs/02-prompt-engineering/llm-prompt-engineering/src/test/java/com/yash/genai/llm_prompt_engineering/LlmPromptEngineeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.genai.llm_prompt_engineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmPromptEngineeringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
