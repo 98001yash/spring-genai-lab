04871adb8ca600cb24d961179af62ee60ff57db6
98001yash <yashchauhan.gaya@gmail.com>
2026-02-14 10:51:53 +0530

    added the llm basics spring boot projects

---
+++ labs/01-llm-basics/llm-basics/src/main/java/com/yash/genai/llm_basics/LlmBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.yash.genai.llm_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmBasicsApplication.class, args);
+	}
+
+}
