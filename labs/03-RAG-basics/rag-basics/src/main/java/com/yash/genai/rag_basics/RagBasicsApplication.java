626db7fb5b590371f10efcfa457fd513c6611100
98001yash <yashchauhan.gaya@gmail.com>
2026-02-15 09:14:17 +0530

    added the RAG basics project i,e  the 03 labs

---
+++ labs/03-RAG-basics/rag-basics/src/main/java/com/yash/genai/rag_basics/RagBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.yash.genai.rag_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagBasicsApplication.class, args);
+	}
+
+}
