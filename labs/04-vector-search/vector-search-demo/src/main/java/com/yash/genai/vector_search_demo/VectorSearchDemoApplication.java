013d9638fa8c99072d7e437c2adb354ae33a1fda
98001yash <yashchauhan.gaya@gmail.com>
2026-02-15 10:32:04 +0530

    added the new spring boot project for the lab 04 vector-search-demo

---
+++ labs/04-vector-search/vector-search-demo/src/main/java/com/yash/genai/vector_search_demo/VectorSearchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yash.genai.vector_search_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectorSearchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VectorSearchDemoApplication.class, args);
+	}
+
+}
