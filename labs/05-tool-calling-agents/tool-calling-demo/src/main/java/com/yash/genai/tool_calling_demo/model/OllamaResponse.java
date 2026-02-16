4f0d85b6483042b32b19c4d5e3d594f53636a835
98001yash <yashchauhan.gaya@gmail.com>
2026-02-16 09:17:41 +0530

    added the model package i.e ollamaRequest and ollamaResponse class

---
+++ labs/05-tool-calling-agents/tool-calling-demo/src/main/java/com/yash/genai/tool_calling_demo/model/OllamaResponse.java
@@ -0,0 +1,14 @@
+package com.yash.genai.tool_calling_demo.model;
+
+public class OllamaResponse {
+
+    private String response;
+
+    public String getResponse(){
+        return response;
+    }
+
+    public void setResponse(String response){
+        this.response = response;
+    }
+}
