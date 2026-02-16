74be96e677962b76461e0fd515923faee6c61937
98001yash <yashchauhan.gaya@gmail.com>
2026-02-16 09:16:03 +0530

    added the config for the webclient and ollama preoperties

---
+++ labs/05-tool-calling-agents/tool-calling-demo/src/main/java/com/yash/genai/tool_calling_demo/config/OllamaProperties.java
@@ -0,0 +1,28 @@
+package com.yash.genai.tool_calling_demo.config;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix="ollama")
+public class OllamaProperties {
+
+    private String baseUrl;
+    private String model;
+
+    public  String getBaseUrl(){
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl){
+        this.baseUrl = baseUrl;
+    }
+    public String getModel(){
+        return model;
+    }
+    public void setModel(String model){
+        this.model = model;
+    }
+
+}
