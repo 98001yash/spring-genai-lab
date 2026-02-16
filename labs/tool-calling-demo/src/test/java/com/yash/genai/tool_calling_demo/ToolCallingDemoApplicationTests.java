dd5dcc001fe64095be01212fb3b88c2fca88fa51
98001yash <yashchauhan.gaya@gmail.com>
2026-02-16 09:04:54 +0530

    added the lab 04 spring boot projects

---
+++ labs/tool-calling-demo/src/test/java/com/yash/genai/tool_calling_demo/ToolCallingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.genai.tool_calling_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolCallingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
